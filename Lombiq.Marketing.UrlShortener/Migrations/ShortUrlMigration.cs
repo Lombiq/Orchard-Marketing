1f41c3c69c0bd978f55eca84362aab3ba5b070f1
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-03-16 17:44:52 +0100

    Add utm part

--- Lombiq.Marketing.UrlShortener/Migrations/ShortUrlMigration.cs
+++ Lombiq.Marketing.UrlShortener/Migrations/ShortUrlMigration.cs
@@ -22,6 +22,46 @@ public sealed class ShortUrlMigration : DataMigration
 
     public async Task<int> CreateAsync()
     {
+        await _contentDefinitionManager.AlterPartDefinitionAsync(nameof(UtmPart), part => part
+            .WithField(nameof(UtmPart.UtmSource), field => field
+                .OfType(nameof(TextField))
+                .WithDisplayName("UTM Source")
+                .WithSettings(new TextFieldSettings
+                {
+                    Hint = "The UTM source. It is used to identify the source of the traffic (for example: google, newsletter, etc.).",
+                }))
+            .WithField(nameof(UtmPart.UtmMedium), field => field
+                .OfType(nameof(TextField))
+                .WithDisplayName("UTM Medium")
+                .WithSettings(new TextFieldSettings
+                {
+                    Hint = "The UTM medium. It is used to identify the medium of the traffic (for example: cpc, email, etc.).",
+                }))
+            .WithField(nameof(UtmPart.UtmCampaign), field => field
+                .OfType(nameof(TextField))
+                .WithDisplayName("UTM Campaign")
+                .WithSettings(new TextFieldSettings
+                {
+                    Hint = "The UTM campaign. It is used to identify the campaign of the traffic (for example: summer-sale, etc.).",
+                }))
+            .WithField(nameof(UtmPart.UtmContent), field => field
+                .OfType(nameof(TextField))
+                .WithDisplayName("UTM Content")
+                .WithSettings(new TextFieldSettings
+                {
+                    Hint = "The UTM content. It is used to identify the content of the traffic (for example: banner, link, etc.).",
+                }))
+            .WithField(nameof(UtmPart.Term), field => field
+                .OfType(nameof(TextField))
+                .WithDisplayName("UTM Term")
+                .WithSettings(new TextFieldSettings
+                {
+                    Hint = "The UTM term. Only relevant for paid ads and is used to filter for search terms used by visitors." +
+                        " It is used to identify the term of the traffic (for example: shoes, etc.).",
+                }))
+        );
+
+
         await _contentDefinitionManager.AlterPartDefinitionAsync(nameof(ShortUrlPart), part => part
             .WithField(nameof(ShortUrlPart.ShortUrl), field => field
                 .OfType(nameof(TextField))
@@ -42,10 +82,12 @@ public sealed class ShortUrlMigration : DataMigration
 
         await _contentDefinitionManager.AlterTypeDefinitionAsync(ContentTypes.ShortUrl, type => type
             .DisplayedAs("Short URL")
+            .Securable()
             .Creatable()
             .Listable()
             .WithPart<TitlePart>()
-            .WithPart<ShortUrlPart>());
+            .WithPart<ShortUrlPart>()
+            .WithPart<UtmPart>());
 
         await SchemaBuilder.CreateMapIndexTableAsync<ShortUrlPartIndex>(table => table
             .Column<string>(nameof(ShortUrlPartIndex.ShortUrl))
