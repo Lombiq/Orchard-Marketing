e40d1f0b6943aaa4306ae86fa1aa857e6a0cae8c
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-03-25 11:31:09 +0100

    Update manifest

--- Lombiq.Marketing.Pirsch/Manifest.cs
+++ Lombiq.Marketing.Pirsch/Manifest.cs
@@ -2,18 +2,20 @@ using Lombiq.Marketing.Pirsch.Constants;
 using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
-    Name = "Lombiq Marketing",
+    Name = "Lombiq Marketing - Pirsch",
     Author = "Lombiq Technologies",
     Version = "0.0.1",
-    Description = "",
+    Description = "Integrate the basics of Pirsch into Orchard Core.",
     Website = "https://github.com/Lombiq/Open-Source-Orchard-Core-Extensions"
 )]
 
 [assembly: Feature(
     Id = FeatureIds.Base,
-    Name = "Lombiq Marketing",
+    Name = "Lombiq Marketing - Pirsch",
     Category = "Marketing",
-    Description = "",
+    Description = "Integrate the basics of Pirsch into Orchard Core.",
     Dependencies =
-    []
+    [
+        Lombiq.Marketing.UrlShortener.Constants.FeatureIds.Base,
+    ]
 )]
