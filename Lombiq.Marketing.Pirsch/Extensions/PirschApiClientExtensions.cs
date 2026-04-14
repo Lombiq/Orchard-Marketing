d7b769293c63cda1f26a18bad2998f73e7086e25
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-04-14 13:55:35 +0200

    Update Lombiq.Marketing.Pirsch/Extensions/PirschApiClientExtensions.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- Lombiq.Marketing.Pirsch/Extensions/PirschApiClientExtensions.cs
+++ Lombiq.Marketing.Pirsch/Extensions/PirschApiClientExtensions.cs
@@ -5,7 +5,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Lombiq.Marketing.Pirsch.Extensions;
+namespace Lombiq.Marketing.Pirsch.Services;
 
 public static class PirschApiClientExtensions
 {
