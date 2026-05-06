eaadb95d05eee91e84b0905983932fe384ee7cc1
Márk Bartha <barthamark@gmail.com>
2026-05-06 17:31:01 +0200

    Polish short URL admin display

--- Lombiq.Marketing.UrlShortener/Services/UrlShorteningService.cs
+++ Lombiq.Marketing.UrlShortener/Services/UrlShorteningService.cs
@@ -12,7 +12,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using YesSql;
-using ISession = YesSql.ISession;
 
 namespace Lombiq.Marketing.UrlShortener.Services;
 
@@ -24,10 +23,7 @@ public class UrlShorteningService : IUrlShorteningService
     private readonly IMemoryCache _memoryCache;
     private readonly ISignal _signal;
 
-    public UrlShorteningService(
-        ISession session,
-        IMemoryCache memoryCache,
-        ISignal signal)
+    public UrlShorteningService(ISession session, IMemoryCache memoryCache, ISignal signal)
     {
         _session = session;
         _memoryCache = memoryCache;
